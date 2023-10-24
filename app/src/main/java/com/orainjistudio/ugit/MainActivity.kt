936de01959a12903b58128ccfbc1228cd4ef99ed
zharfanwafiq <zharfanww@gmail.com>
2023-10-24 11:28:53 +0700

    first commit

---
+++ app/src/main/java/com/orainjistudio/ugit/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.orainjistudio.ugit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
